a12631b8515c87274ec140c56e63c372e29231a9
emmanuel <emmanuelamedu5@gmail.com>
2023-02-08 16:39:20 +0100

    Added my project

--- Banking/src/main/java/com/BankingApplication/Banking/configuration/appConfig.java
+++ Banking/src/main/java/com/BankingApplication/Banking/configuration/appConfig.java
@@ -1,2 +1,19 @@
-package com.BankingApplication.Banking.configuration;public class appConfig {
+package com.BankingApplication.Banking.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@ComponentScan(basePackages = {"com.BankingApplication.Banking"})
+public class appConfig extends WebMvcConfigurationSupport {
+
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+ }
 }
